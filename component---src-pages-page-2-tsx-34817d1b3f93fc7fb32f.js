3849887272e7e689b86f00c98d9da9bbd1b683c1
Joey Figaro <joey@joeyfigaro.com>
2020-01-31 09:19:26 -0500

    Updates

---
+++ component---src-pages-page-2-tsx-34817d1b3f93fc7fb32f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{BV7U:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),o=a.n(n),l=a("Wbzz"),c=a("soUV"),r=a("vrFN");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-34817d1b3f93fc7fb32f.js.map

44a0b8e2ebf2b9398737e99794f8af130c136add
Joey Figaro <joey@joeyfigaro.com>
2020-02-05 12:25:00 -0500

    added initial preview image and configuration

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { IconContext } from 'react-icons'
+import Helmet from 'react-helmet'
 import cx from 'classnames'
 import { useStaticQuery, graphql } from 'gatsby'
 
@@ -35,11 +36,29 @@ const Layout = ({
     <IconContext.Provider
       value={{ color: 'white', style: { width: 21, height: 21 } }}
     >
-      {/*
-      <LayoutContext.Provider
-        value={{ sidePanelVisible, closeSidePanel, openSidePanel }}
-      >
-    */}
+      <Helmet>
+        <title>
+          Mobile and Web Development — Personal Showcase | Joey Figaro
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          property="og:title"
+          content="Mobile and Web Development — Personal Showcase | Joey Figaro"
+        />
+        <meta
+          property="og:description"
+          content="I'm an RVA native who lives to build interfaces and applications for the web, mobile, and everything in-between."
+        />
+        <meta
+          property="og:image"
+          content="https://joeyfigaro.com/generated-preview.png"
+        />
+        <meta property="og:url" content="https://joeyfigaro.com" />
+        <meta name="twitter:card" content="summary" />
+        <meta property="og:type" content="article" />
+        <meta property="og:locale" content="en_US" />
+        <link rel="canonical" href="https://joeyfigaro.com" />
+      </Helmet>
       <div className={cx(styles.Perspective)}>
         <main className={styles.Content}>
           {children}
